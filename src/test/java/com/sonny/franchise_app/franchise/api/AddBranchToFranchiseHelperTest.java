c49354f9a1f4caa2540aa22d8ef754d3154d1b3d
sonny <sonny.7n@gmail.com>
2025-11-20 22:53:19 -0500

    Se agregan las pruebas unitarias para add branch to franchise

---
+++ src/test/java/com/sonny/franchise_app/franchise/api/AddBranchToFranchiseHelperTest.java
@@ -0,0 +1,93 @@
+package com.sonny.franchise_app.franchise.api;
+
+import com.sonny.franchise_app.branch.entity.Branch;
+import com.sonny.franchise_app.branch.exception.BranchDuplicatedNameException;
+import com.sonny.franchise_app.branch.repository.BranchRepository;
+import com.sonny.franchise_app.branch.stub.BranchTestStub;
+import com.sonny.franchise_app.franchise.exception.FranchiseNotFoundException;
+import com.sonny.franchise_app.franchise.repository.FranchiseRepository;
+import com.sonny.franchise_app.franchise.request.AddBranchRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class AddBranchToFranchiseHelperTest {
+
+    @Mock
+    private FranchiseRepository franchiseRepository;
+
+    @Mock
+    private BranchRepository branchRepository;
+
+    @InjectMocks
+    private AddBranchToFranchiseHelper helper;
+
+    @Test
+    void addNewBranchWithFranchiseNotExist() {
+
+        AddBranchRequest request = BranchTestStub.getDefaultBranchRequest();
+
+        when(franchiseRepository.existsById(request.getFranchiseId()))
+                .thenReturn(Mono.just(false)); //
+
+        StepVerifier.create(helper.addNewBranchToFranchise(request))
+                .expectErrorMatches(error ->
+                        error instanceof FranchiseNotFoundException &&
+                                error.getMessage().contains(String.valueOf(request.getFranchiseId()))
+                )
+                .verify();
+    }
+
+    @Test
+    void addNewBranchWithBranchNameExists() {
+
+        AddBranchRequest request = BranchTestStub.getDefaultBranchRequest();
+        when(franchiseRepository.existsById(request.getFranchiseId()))
+                .thenReturn(Mono.just(true));
+
+        when(branchRepository.existsByName(request.getName()))
+                .thenReturn(Mono.just(true));
+
+        StepVerifier.create(helper.addNewBranchToFranchise(request))
+                .expectErrorMatches(error ->
+                        error instanceof BranchDuplicatedNameException &&
+                                error.getMessage().contains(request.getName())
+                )
+                .verify();
+    }
+
+    @Test
+    void addNewBranch_WhenValid_ShouldSaveAndReturnDto() {
+
+        AddBranchRequest request = BranchTestStub.getDefaultBranchRequest();
+
+        Branch saved = BranchTestStub.getDefaultBranch();
+
+        when(franchiseRepository.existsById(request.getFranchiseId()))
+                .thenReturn(Mono.just(true));
+
+        when(branchRepository.existsByName(request.getName()))
+                .thenReturn(Mono.just(false));
+
+        when(branchRepository.save(any(Branch.class)))
+                .thenReturn(Mono.just(saved));
+
+        StepVerifier.create(helper.addNewBranchToFranchise(request))
+                .assertNext(dto -> {
+                    assertEquals(saved.getId(), dto.getId());
+                    assertEquals(saved.getName(), dto.getName());
+                    assertEquals(saved.getFranchiseId(), dto.getFranchiseId());
+                })
+                .verifyComplete();
+    }
+
+}

a0515eb7c8a3667db0e275f5a2b64f197537d182
sonny <sonny.7n@gmail.com>
2025-11-21 10:50:23 -0500

    Se agrega funcionalidad para actualizar stock de un producto con pruebass unitarias

---
+++ src/test/java/com/sonny/franchise_app/product/api/ProductUpdaterTest.java
@@ -0,0 +1,75 @@
+package com.sonny.franchise_app.product.api;
+
+import com.sonny.franchise_app.product.dto.ProductDto;
+import com.sonny.franchise_app.product.entity.Product;
+import com.sonny.franchise_app.product.exception.ProductNotFoundException;
+import com.sonny.franchise_app.product.repository.ProductRepository;
+import com.sonny.franchise_app.product.stub.ProductTestStub;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(SpringExtension.class)
+public class ProductUpdaterTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductUpdater productUpdater;
+
+
+    @Test
+    void whenUpdateStockWithProductExistsThenUpdateAndReturnDto() {
+
+        Long id = 1L;
+        int newStock = 50;
+
+        Product existingProduct = ProductTestStub.getDefaultProduct();
+        Product updatedProduct = ProductTestStub.
+                getProductWithIdAndStock(existingProduct.getId(), newStock);
+
+        ProductDto updatedDto = ProductTestStub.getProductDto();
+        updatedDto.setStock(newStock);
+
+
+        when(productRepository.findById(id))
+                .thenReturn(Mono.just(existingProduct));
+
+        when(productRepository.save(existingProduct))
+                .thenReturn(Mono.just(updatedProduct));
+
+        Mono<ProductDto> result = productUpdater.updateStock(id, newStock);
+
+        StepVerifier.create(result)
+                .expectNextMatches(dto ->
+                        dto.getId().equals(id) &&
+                                dto.getStock() == newStock)
+                .verifyComplete();
+    }
+
+    @Test
+    void whenUpdateStockWithProductNotFoundThenThrowException() {
+        Long id = 99L;
+        int newStock = 20;
+
+        when(productRepository.findById(id))
+                .thenReturn(Mono.empty());
+
+        Mono<ProductDto> result = productUpdater.updateStock(id, newStock);
+
+        StepVerifier.create(result)
+                .expectError(ProductNotFoundException.class)
+                .verify();
+
+        verify(productRepository).findById(id);
+        verify(productRepository, never()).save(any());
+    }
+}

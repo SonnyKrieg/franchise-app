0bba314cd6a3ce3e2122d1f1d9ee5129f4602f30
sonny <sonny.7n@gmail.com>
2025-11-20 22:43:05 -0500

    Se agrega la clase para la creación de la sucursal. Se agrega el dto de sucursal. Se agrega el repositorio de la sucursal. Se agrega una exception para franquicia no encontrada

---
+++ src/main/java/com/sonny/franchise_app/franchise/api/AddBranchToFranchiseHelper.java
@@ -0,0 +1,47 @@
+package com.sonny.franchise_app.franchise.api;
+
+import com.sonny.franchise_app.branch.dto.BranchDto;
+import com.sonny.franchise_app.branch.repository.BranchRepository;
+import com.sonny.franchise_app.franchise.exception.FranchiseNotFoundException;
+import com.sonny.franchise_app.franchise.repository.FranchiseRepository;
+import com.sonny.franchise_app.franchise.request.AddBranchRequest;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class AddBranchToFranchiseHelper {
+
+    private final FranchiseRepository franchiseRepository;
+    private final BranchRepository branchRepository;
+
+    public Mono<BranchDto> addNewBranchToFranchise(AddBranchRequest request) {
+
+        return franchiseRepository.existsById(request.getFranchiseId())
+                .flatMap(existsFranchise -> {
+                    if (!existsFranchise)
+                        return Mono.error(new FranchiseNotFoundException(request.getFranchiseId()));
+                    return branchRepository.existsByName(request.getName());
+                })
+                .flatMap(exists -> {
+                    if (exists) {
+                        return Mono.error(
+                                new BranchDuplicatedNameException(request.getName()));
+                    }
+
+                    Branch branch = Branch.builder()
+                            .name(request.getName())
+                            .franchiseId(request.getFranchiseId())
+                            .build();
+                    return branchRepository.save(branch);
+                })
+                .map(BranchMapper::toDto)
+                .doOnSuccess(f -> log.info("La sucursal se guardó correctamente en la base de datos."))
+                .doOnError(e -> log.error("Error al guardar la sucursal", e));
+    }
+
+}
---
+++ src/main/java/com/sonny/franchise_app/franchise/request/AddBranchRequest.java
@@ -0,0 +1,24 @@
+package com.sonny.franchise_app.franchise.request;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class AddBranchRequest {
+
+    @NotBlank(message = "Debe escribir el nombre de la sucursal.")
+    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
+    @Pattern(regexp = "^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$", message = "El nombre solo puede contener letras y espacios")
+    private String name;
+
+    @NotNull(message = "Debe escribir el Id de la franquicia.")
+    private Long franchiseId;
+}

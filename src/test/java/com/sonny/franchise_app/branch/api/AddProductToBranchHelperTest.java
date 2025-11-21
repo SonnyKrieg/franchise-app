4564ee480d918600709c1461015c7d9393d8ac4f
sonny <sonny.7n@gmail.com>
2025-11-21 07:57:03 -0500

    Se agregan las pruebas unitarias para la funcionalidad agregar producto

---
+++ src/test/java/com/sonny/franchise_app/branch/api/AddProductToBranchHelperTest.java
@@ -0,0 +1,94 @@
+package com.sonny.franchise_app.branch.api;
+
+import com.sonny.franchise_app.branch.exception.BranchNotFoundException;
+import com.sonny.franchise_app.branch.repository.BranchRepository;
+import com.sonny.franchise_app.branch.request.AddProductRequest;
+import com.sonny.franchise_app.product.entity.Product;
+import com.sonny.franchise_app.product.exception.ProductDuplicateNameException;
+import com.sonny.franchise_app.product.repository.ProductRepository;
+import com.sonny.franchise_app.product.stub.ProductTestStub;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class AddProductToBranchHelperTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Mock
+    private BranchRepository branchRepository;
+
+    @InjectMocks
+    private AddProductToBranchHelper helper;
+
+    @Test
+    void addNewProductWithBranchNotExist() {
+
+        AddProductRequest request = ProductTestStub.getDefaultProductRequest();
+
+        when(branchRepository.existsById(request.getBranchId()))
+                .thenReturn(Mono.just(false)); //
+
+        StepVerifier.create(helper.addNewProductToBranch(request))
+                .expectErrorMatches(error ->
+                        error instanceof BranchNotFoundException &&
+                                error.getMessage().contains(String.valueOf(request.getBranchId()))
+                )
+                .verify();
+    }
+
+    @Test
+    void addNewProductWithProductNameExists() {
+
+        AddProductRequest request = ProductTestStub.getDefaultProductRequest();
+        when(branchRepository.existsById(request.getBranchId()))
+                .thenReturn(Mono.just(true));
+
+        when(productRepository.existsByName(request.getName()))
+                .thenReturn(Mono.just(true));
+
+        StepVerifier.create(helper.addNewProductToBranch(request))
+                .expectErrorMatches(error ->
+                        error instanceof ProductDuplicateNameException &&
+                                error.getMessage().contains(request.getName())
+                )
+                .verify();
+    }
+
+    @Test
+    void addNewProductValid() {
+
+        AddProductRequest request = ProductTestStub.getDefaultProductRequest();
+
+        Product saved = ProductTestStub.getDefaultProduct();
+
+        when(branchRepository.existsById(request.getBranchId()))
+                .thenReturn(Mono.just(true));
+
+        when(productRepository.existsByName(request.getName()))
+                .thenReturn(Mono.just(false));
+
+        when(productRepository.save(any(Product.class)))
+                .thenReturn(Mono.just(saved));
+
+        StepVerifier.create(helper.addNewProductToBranch(request))
+                .assertNext(dto -> {
+                    assertEquals(saved.getId(), dto.getId());
+                    assertEquals(saved.getName(), dto.getName());
+                    assertEquals(saved.getBranchId(), dto.getBranchId());
+                    assertEquals(saved.getStock(), dto.getStock());
+                })
+                .verifyComplete();
+    }
+
+}
